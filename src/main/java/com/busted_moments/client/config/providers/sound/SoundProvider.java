96a33ab52b7b783f39d9a03f65c1ec3621272a7e
Essentuan <51426319+Essentuan@users.noreply.github.com>
2023-08-31 23:47:44 -0500

    Add support for custom sounds

--- src/main/java/com/busted_moments/client/config/providers/sound/SoundProvider.java
+++ src/main/java/com/busted_moments/client/config/providers/sound/SoundProvider.java
@@ -1,19 +1,24 @@
 package com.busted_moments.client.config.providers.sound;
 
+import com.busted_moments.core.FuyExtension;
 import com.busted_moments.core.config.Config;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SoundProvider implements Config.Dropdown.Provider<SoundEvent> {
-   private static final Map<String, SoundEvent> SOUNDS = BuiltInRegistries.SOUND_EVENT.stream().collect(Collectors.toMap(
-           event -> event.getLocation().toString(),
-           event -> event
-   ));
+   private static final Map<String, SoundEvent> SOUNDS =
+           Stream.concat(FabricLoader.getInstance().getEntrypointContainers("fuy_gg", FuyExtension.class).stream()
+                           .flatMap(extension -> Stream.of(extension.getEntrypoint().getSounds()))
+                           .map(location -> SoundEvent.createVariableRangeEvent(new ResourceLocation(location))), BuiltInRegistries.SOUND_EVENT.stream())
+                   .collect(Collectors.toMap(event -> event.getLocation().toString(), event -> event));
 
    @Override
    public Iterable<SoundEvent> getOptions() {
--- src/main/java/com/busted_moments/core/FuyExtension.java
+++ src/main/java/com/busted_moments/core/FuyExtension.java
@@ -2,4 +2,9 @@ package com.busted_moments.core;
 
 public interface FuyExtension {
    String getPackage();
+
+   //This is mainly for SoundProvider
+   default String[] getSounds() {
+      return new String[0];
+   }
 }
