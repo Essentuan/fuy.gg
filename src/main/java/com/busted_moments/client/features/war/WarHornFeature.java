11b863045e9db985d438a21ddf1f2a7615dcbab4
Tamás <gszabotomi@gmail.com>
2023-09-01 15:32:35 +0200

    Fixed it sending the sound twice

--- src/main/java/com/busted_moments/client/features/war/WarHornFeature.java
+++ src/main/java/com/busted_moments/client/features/war/WarHornFeature.java
@@ -4,6 +4,8 @@ import com.busted_moments.client.config.providers.sound.SoundProvider;
 import com.busted_moments.client.util.SoundUtil;
 import com.busted_moments.core.Feature;
 import com.busted_moments.core.config.Config;
+import com.busted_moments.core.time.Duration;
+import com.busted_moments.core.time.TimeUnit;
 import com.wynntils.core.text.PartStyle;
 import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
 import com.wynntils.handlers.chat.type.RecipientType;
@@ -11,12 +13,14 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Matcher;
 
 import static com.busted_moments.client.models.war.timer.TimerModel.ATTACK_PATTERN;
 
 @Config.Category("War")
-@Feature.Definition(name = "Autoskip Dialogue", description = "Automatically skips dialogue")
+@Feature.Definition(name = "War Horn", description = "Plays a sound when a war starts")
 public class WarHornFeature extends Feature {
     @Dropdown(title = "Selected Sound", options = SoundProvider.class)
     private SoundEvent selected = SoundEvents.ANVIL_LAND;
@@ -30,10 +34,11 @@ public class WarHornFeature extends Feature {
     private static Float pitch = 1.0F;
     @SubscribeEvent
     public void onMessage(ChatMessageReceivedEvent event) {
-        if (event.getRecipientType() != RecipientType.GUILD) return;
 
         Matcher matcher = event.getOriginalStyledText().getMatcher(ATTACK_PATTERN, PartStyle.StyleType.NONE);
         if (!matcher.matches()) return;
+        String Time = matcher.group("timer");
+        if (Objects.equals(Time, "30 seconds")) return;
 
         SoundUtil.playAmbient(selected, pitch, volume);
     }
