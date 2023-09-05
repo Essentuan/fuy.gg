68f57ecc09d82b241af073788b12052b0ab970f3
Essentuan <51426319+Essentuan@users.noreply.github.com>
2023-09-04 20:08:34 -0500

    Improve ClickToCongratulate

--- src/main/java/com/busted_moments/client/features/ClickToCongratulateFeature.java
+++ src/main/java/com/busted_moments/client/features/ClickToCongratulateFeature.java
@@ -4,39 +4,41 @@ import com.busted_moments.client.util.ChatUtil;
 import com.busted_moments.core.Default;
 import com.busted_moments.core.Feature;
 import com.busted_moments.core.State;
-import com.busted_moments.core.heartbeat.Heartbeat;
-import com.busted_moments.core.time.TimeUnit;
+import com.busted_moments.core.text.TextBuilder;
+import com.wynntils.core.components.Managers;
+import com.wynntils.core.text.PartStyle;
 import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.ClickEvent;
-import net.minecraft.network.chat.Style;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import static com.busted_moments.client.util.ChatUtil.component;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static net.minecraft.ChatFormatting.AQUA;
 
 @Default(State.ENABLED)
 @Feature.Definition(name = "Click To Congratulate", description = "Click to send a congratulations message!")
 public class ClickToCongratulateFeature extends Feature {
+   private static final Pattern CONGRATULATIONS_PATTERN = Pattern.compile("^\\[!\\] Congratulations to (?<player>.+) for reaching (?<type>.+) level (?<level>.+)!");
 
-    @Value("Congratulations Message")
-    @Tooltip("What message to use to congratulate people")
-    private static String CongratsMSG = "Congratulations!";
+   @Value("Congratulations Message")
+   @Tooltip("What message to use to congratulate people")
+   private static String message = "Congratulations!";
 
-    @SubscribeEvent
-    public void ChatMessageReceivedEvent(ChatMessageReceivedEvent event){
-        String message = event.getMessage().getString();
-        if (!message.contains("[!] Congratulations to")) return;
-        String PlayerName = message.split(" ")[3];
+   @SubscribeEvent
+   public void ChatMessageReceivedEvent(ChatMessageReceivedEvent event) {
+      Matcher matcher = event.getOriginalStyledText().getMatcher(CONGRATULATIONS_PATTERN, PartStyle.StyleType.NONE);
+      if (!matcher.matches()) return;
 
-        Heartbeat.schedule(() -> {
-            ChatUtil.message(
-                    (component(
-                            "Click to congratulate %s!".formatted(PlayerName)
-                    ).setStyle(
-                            Style.EMPTY.withClickEvent(
-                                            new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/msg %s %s".formatted(PlayerName, CongratsMSG)))
-                                    .withColor(ChatFormatting.AQUA)))
-            );
-        }, 10, TimeUnit.MILLISECONDS);
-    }
+      String player = matcher.group("player");
+
+      Managers.TickScheduler.scheduleNextTick(() -> ChatUtil.message(
+              TextBuilder.of("Click to congratulate ", AQUA)
+                      .append(player, AQUA).append("!", AQUA)
+                      .onClick(
+                              ClickEvent.Action.RUN_COMMAND,
+                              "/msg %s %s".formatted(player, message)
+                      )
+      ));
+   }
 }
