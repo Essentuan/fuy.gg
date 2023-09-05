61eb3f0883fe84521193289d1b80609ea9eee25b
Essentuan <51426319+Essentuan@users.noreply.github.com>
2023-09-04 21:04:58 -0500

    Enable War Horn by default

--- src/main/java/com/busted_moments/client/features/war/WarHornFeature.java
+++ src/main/java/com/busted_moments/client/features/war/WarHornFeature.java
@@ -4,11 +4,14 @@ import com.busted_moments.client.config.providers.sound.SoundProvider;
 import com.busted_moments.client.models.war.timer.events.TimerStartEvent;
 import com.busted_moments.client.util.SoundUtil;
 import com.busted_moments.client.util.Sounds;
+import com.busted_moments.core.Default;
 import com.busted_moments.core.Feature;
+import com.busted_moments.core.State;
 import com.busted_moments.core.config.Config;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+@Default(State.ENABLED)
 @Config.Category("War")
 @Feature.Definition(name = "War Horn", description = "Plays a sound when a war starts")
 public class WarHornFeature extends Feature {
