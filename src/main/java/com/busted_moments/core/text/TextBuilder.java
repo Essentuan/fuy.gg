0cc39ffa75397e45b95641aae1ec416b97203563
Essentuan <51426319+Essentuan@users.noreply.github.com>
2023-10-10 17:33:16 -0500

    feat: Extend TextBuilder

--- src/main/java/com/busted_moments/core/text/TextBuilder.java
+++ src/main/java/com/busted_moments/core/text/TextBuilder.java
@@ -6,9 +6,12 @@ import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public interface TextBuilder {
    TextBuilder append(StyledText text);
@@ -20,6 +23,25 @@ public interface TextBuilder {
    }
 
    TextBuilder append(String string, ChatFormatting... styles);
+
+   default TextBuilder append(String format, Object... args) {
+      List<ChatFormatting> formattings = new ArrayList<>();
+
+      return append(format.formatted(
+              Stream.of(args).filter(obj -> {
+                 if (obj instanceof ChatFormatting formatting) {
+                    formattings.add(formatting);
+                    return false;
+                 } else return true;
+              }).map(obj -> {
+                 if (obj instanceof Component component)
+                    return component.getString();
+                 else if (obj instanceof StyledText text)
+                    return text.getString();
+                 else return obj;
+              }).toArray()
+      ), formattings.toArray(ChatFormatting[]::new));
+   }
 
    default TextBuilder append(Object string, ChatFormatting... styles) {
       return append(String.valueOf(string), styles);
@@ -51,12 +73,37 @@ public interface TextBuilder {
       return this;
    }
 
-
-   default TextBuilder appendIf(Supplier<Boolean> condition, String string, ChatFormatting... styles) {
-      if (condition.get()) append(string, styles);
+   default TextBuilder appendIf(boolean condition, String string, ChatFormatting... styles) {
+      if (condition) append(string, styles);
 
       return this;
    }
+
+
+   default TextBuilder appendIf(Supplier<Boolean> condition, String string, ChatFormatting... styles) {
+      return appendIf(condition.get(), string, styles);
+   }
+
+   default TextBuilder appendIf(boolean condition, String format, Object... args) {
+      if (condition) append(format, args);
+
+      return this;
+   }
+
+   default TextBuilder appendIf(boolean condition, String format, Supplier<Object[]> args, ChatFormatting... styles) {
+      if (condition) append(format.formatted(args.get()), styles);
+
+      return this;
+   }
+
+   default TextBuilder appendIf(Supplier<Boolean> condition, String format, Object... args) {
+      return appendIf(condition.get(), format, args);
+   }
+
+   default TextBuilder appendIf(Supplier<Boolean> condition, String format, Supplier<Object[]> args, ChatFormatting... styles) {
+      return appendIf(condition.get(), format, args.get(), styles);
+   }
+
 
    TextBuilder prepend(StyledText text);
 
@@ -72,27 +119,35 @@ public interface TextBuilder {
 
    default <T> TextBuilder onHover(HoverEvent.Action<T> action, T obj) {
       return onHover(new HoverEvent(action, obj));
-   };
+   }
+
+   ;
 
    default TextBuilder onHover(Consumer<TextBuilder> consumer) {
       TextBuilder builder = TextBuilder.empty();
       consumer.accept(builder);
 
       return onHover(HoverEvent.Action.SHOW_TEXT, builder.toComponent());
-   };
+   }
+
+   ;
 
    TextBuilder onPartHover(HoverEvent event);
 
    default <T> TextBuilder onPartHover(HoverEvent.Action<T> action, T obj) {
       return onPartHover(new HoverEvent(action, obj));
-   };
+   }
+
+   ;
 
    default TextBuilder onPartHover(Consumer<TextBuilder> consumer) {
       TextBuilder builder = TextBuilder.empty();
       consumer.accept(builder);
 
       return onPartHover(HoverEvent.Action.SHOW_TEXT, builder.toComponent());
-   };
+   }
+
+   ;
 
 
    TextBuilder onClick(ClickEvent event);
