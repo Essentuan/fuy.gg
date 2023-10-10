2939ec0413e62c764294875f790cecacbb4aa475
Essentuan <51426319+Essentuan@users.noreply.github.com>
2023-10-10 17:35:34 -0500

    feat: Move isNear & isInside to EntityUtil

--- src/main/java/com/busted_moments/client/util/PlayerUtil.java
+++ src/main/java/com/busted_moments/client/util/PlayerUtil.java
@@ -1,9 +1,7 @@
 package com.busted_moments.client.util;
 
-import com.wynntils.utils.mc.McUtils;
 import net.minecraft.core.Position;
 import net.minecraft.world.effect.MobEffect;
-import org.apache.commons.lang3.Range;
 
 import static com.wynntils.utils.mc.McUtils.player;
 
@@ -13,20 +11,11 @@ public class PlayerUtil {
    }
 
    public static boolean isNear(Position position, int radius) {
-      var player = player();
-
-      return player != null && player.position().closerThan(position, radius);
+      return EntityUtil.isNear(player(), position, radius);
    }
 
    public static boolean isInside(Position pos1, Position pos2) {
-      var player = player();
-      if (player == null) return false;
-
-      var loc = player.position();
-
-      return Range.of(pos1.x(), pos2.x()).contains(loc.x) &&
-              Range.of(pos1.y(), pos2.y()).contains(loc.y) &&
-              Range.of(pos1.z(), pos2.z()).contains(loc.z);
+      return EntityUtil.isInside(player(), pos1, pos2);
    }
 
    public static boolean hasEffect(MobEffect effect) {
