32a4d7f8882a6970b0ac63b425c2977add10a493
Tamás <gszabotomi@gmail.com>
2023-10-22 04:32:26 +0200

    Fix AutoSkipDialogueFeature

--- src/main/java/com/busted_moments/client/features/AutoSkipDialogueFeature.java
+++ src/main/java/com/busted_moments/client/features/AutoSkipDialogueFeature.java
@@ -4,6 +4,7 @@ import com.busted_moments.client.util.PacketUtil;
 import com.busted_moments.core.Feature;
 import com.busted_moments.core.heartbeat.Heartbeat;
 import com.busted_moments.core.time.TimeUnit;
+import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
 import com.wynntils.handlers.chat.event.NpcDialogEvent;
 import com.wynntils.handlers.chat.type.NpcDialogueType;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,6 +16,15 @@ public class AutoSkipDialogueFeature extends Feature {
     @SubscribeEvent
     public void NpcDialogEvent(NpcDialogEvent event) {
         if (event.getType() != NpcDialogueType.NORMAL) return;
+
+        PacketUtil.Action(Action.PRESS_SHIFT_KEY);
+
+        Heartbeat.schedule(() -> PacketUtil.Action(Action.RELEASE_SHIFT_KEY), 50, TimeUnit.MILLISECONDS);
+    }
+
+    @SubscribeEvent
+    public void ChatReceivedEvent(ChatMessageReceivedEvent event){
+        if (!event.getStyledText().contains("§7Press §fSHIFT §7to continue§r")) return;
 
         PacketUtil.Action(Action.PRESS_SHIFT_KEY);
 
