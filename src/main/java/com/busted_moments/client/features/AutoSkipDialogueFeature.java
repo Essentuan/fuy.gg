d8ef4c8de95065a6c60b9cf9b0cc73d8dc69269c
Tamás <gszabotomi@gmail.com>
2023-10-22 05:41:39 +0200

    Small Bug fixed

--- src/main/java/com/busted_moments/client/features/AutoSkipDialogueFeature.java
+++ src/main/java/com/busted_moments/client/features/AutoSkipDialogueFeature.java
@@ -4,6 +4,7 @@ import com.busted_moments.client.util.PacketUtil;
 import com.busted_moments.core.Feature;
 import com.busted_moments.core.heartbeat.Heartbeat;
 import com.busted_moments.core.time.TimeUnit;
+import com.wynntils.core.text.PartStyle;
 import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
 import com.wynntils.handlers.chat.event.NpcDialogEvent;
 import com.wynntils.handlers.chat.type.NpcDialogueType;
@@ -14,6 +15,8 @@ import static net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket
 
 @Feature.Definition(name = "Autoskip Dialogue", description = "Automatically skips dialogue")
 public class AutoSkipDialogueFeature extends Feature {
+    private static final String PRESS_TO_CONTINUE_TEXT = "Press SHIFT to continue";
+
     @SubscribeEvent
     public void NpcDialogEvent(NpcDialogEvent event) {
         if (event.getType() != NpcDialogueType.NORMAL) return;
@@ -25,8 +28,8 @@ public class AutoSkipDialogueFeature extends Feature {
 
     @SubscribeEvent
     public void ChatReceivedEvent(ChatMessageReceivedEvent event){
-        if (event.getRecipientType() == RecipientType.NPC) return;
-
+        if (event.getRecipientType() != RecipientType.INFO || !event.getStyledText().trim().contains(PRESS_TO_CONTINUE_TEXT, PartStyle.StyleType.NONE)) return;
+        
         PacketUtil.Action(Action.PRESS_SHIFT_KEY);
 
         Heartbeat.schedule(() -> PacketUtil.Action(Action.RELEASE_SHIFT_KEY), 50, TimeUnit.MILLISECONDS);
