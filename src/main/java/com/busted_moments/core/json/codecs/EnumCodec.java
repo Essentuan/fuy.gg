150d52c6f4c1f640f5c8a922686c0e30334d92ea
Essentuan <51426319+Essentuan@users.noreply.github.com>
2023-12-15 23:56:41 -0600

    feat: Add EnumUtil.valueOf

--- src/main/java/com/busted_moments/core/json/codecs/EnumCodec.java
+++ src/main/java/com/busted_moments/core/json/codecs/EnumCodec.java
@@ -3,14 +3,12 @@ package com.busted_moments.core.json.codecs;
 import com.busted_moments.core.UnexpectedException;
 import com.busted_moments.core.json.AbstractCodec;
 import com.busted_moments.core.json.Annotations;
+import com.busted_moments.core.util.EnumUtil;
 import com.busted_moments.core.util.Reflection;
-import com.busted_moments.core.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Type;
-import java.util.List;
-import java.util.regex.Pattern;
 
 @SuppressWarnings({"rawtypes"})
 @AbstractCodec.Definition(Enum.class)
@@ -25,28 +23,19 @@ public class EnumCodec extends AbstractCodec<Enum, String> {
       return fromString(value, type, typeArgs);
    }
 
-   public static final Pattern EMPTY_PATTERN = Pattern.compile("[_ -']");
-
    @Override
+   @SuppressWarnings("unchecked")
    public Enum fromString(String string, Class<?> type, Type... typeArgs) {
-      try {
-         return Enum.valueOf((Class<? extends Enum>) type, string);
-      } catch (Exception e) {
-         return StringUtil.bestMatch(
-                 fix(string),
-                 List.of((Enum[]) type.getEnumConstants()),
-                 EnumCodec::fix,
-                 false
-         ).orElseThrow(() -> new UnexpectedException("Couldn't find %s that matches %s", Reflection.toSimpleString(type), string));
-      }
+      Object obj = EnumUtil.valueOf(string, (Class<Enum>) type).orElse(null);
+
+      if (obj == null)
+         throw new UnexpectedException("Couldn't find %s that matches %s", Reflection.toSimpleString(type), string);
+
+      return (Enum) obj;
    }
 
    @Override
    public String toString(Enum value, Class<?> type, Type... typeArgs) {
       return value.toString();
-   }
-
-   public static String fix(Object object) {
-      return EMPTY_PATTERN.matcher(object.toString()).replaceAll("");
    }
 }
