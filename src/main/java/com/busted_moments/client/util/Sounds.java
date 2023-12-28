894bb7b71e87991a84d47f29dcffbec2d7f6976a
Essentuan <51426319+Essentuan@users.noreply.github.com>
2023-12-28 17:39:46 -0600

    feat: Add cheep cheep sound
    Lamama.exe

--- src/main/java/com/busted_moments/client/util/Sounds.java
+++ src/main/java/com/busted_moments/client/util/Sounds.java
@@ -4,4 +4,5 @@ import net.minecraft.sounds.SoundEvent;
 
 public interface Sounds {
    SoundEvent WAR_HORN = SoundUtil.create("fuy:war.horn");
+   SoundEvent CHEEP_CHEEP = SoundUtil.create("fuy:cheep.cheep");
 }
