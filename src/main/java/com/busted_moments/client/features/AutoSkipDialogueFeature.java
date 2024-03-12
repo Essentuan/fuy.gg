853c436d7834a732c289703711533c92e6e13336
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-03-12 13:27:57 -0500

    fix: Fix Artemis compatibility

--- src/main/java/com/busted_moments/client/features/AutoSkipDialogueFeature.java
+++ src/main/java/com/busted_moments/client/features/AutoSkipDialogueFeature.java
@@ -4,11 +4,7 @@ import com.busted_moments.client.util.PacketUtil;
 import com.busted_moments.core.Feature;
 import com.busted_moments.core.heartbeat.Heartbeat;
 import com.busted_moments.core.time.ChronoUnit;
-import com.wynntils.core.text.PartStyle;
-import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
-import com.wynntils.handlers.chat.event.NpcDialogEvent;
-import com.wynntils.handlers.chat.type.NpcDialogueType;
-import com.wynntils.handlers.chat.type.RecipientType;
+import com.wynntils.models.npcdialogue.event.NpcDialogueProcessingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import static net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket.Action;
@@ -18,18 +14,7 @@ public class AutoSkipDialogueFeature extends Feature {
     private static final String PRESS_TO_CONTINUE_TEXT = "Press SHIFT to continue";
 
     @SubscribeEvent
-    public void NpcDialogEvent(NpcDialogEvent event) {
-        if (event.getType() != NpcDialogueType.NORMAL) return;
-
-        PacketUtil.Action(Action.PRESS_SHIFT_KEY);
-
-        Heartbeat.schedule(() -> PacketUtil.Action(Action.RELEASE_SHIFT_KEY), 50, ChronoUnit.MILLISECONDS);
-    }
-
-    @SubscribeEvent
-    public void ChatReceivedEvent(ChatMessageReceivedEvent event){
-        if (event.getRecipientType() != RecipientType.INFO || !event.getStyledText().trim().contains(PRESS_TO_CONTINUE_TEXT, PartStyle.StyleType.NONE)) return;
-        
+    public void NpcDialogEvent(NpcDialogueProcessingEvent.Post event) {
         PacketUtil.Action(Action.PRESS_SHIFT_KEY);
 
         Heartbeat.schedule(() -> PacketUtil.Action(Action.RELEASE_SHIFT_KEY), 50, ChronoUnit.MILLISECONDS);
