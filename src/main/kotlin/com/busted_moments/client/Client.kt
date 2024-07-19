96db9125306da7d2ec5133940795df6de0e4ad8b
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-07-19 17:44:34 -0500

    feat: Add Sound support

--- src/main/kotlin/com/busted_moments/client/Client.kt
+++ src/main/kotlin/com/busted_moments/client/Client.kt
@@ -1,12 +1,20 @@
 package com.busted_moments.client
 
+import com.busted_moments.buster.Buster
 import com.busted_moments.client.framework.Extension
 import com.busted_moments.client.framework.FabricLoader
 import com.busted_moments.client.framework.Scan
 import com.busted_moments.client.framework.config.Config
 import com.busted_moments.client.framework.events.events
+import com.busted_moments.client.framework.artemis.Function
+import com.busted_moments.client.framework.keybind.Keybind
+import com.busted_moments.client.framework.marker.Marker
+import com.busted_moments.client.framework.sounds.Sounds
+import com.wynntils.models.marker.type.MarkerProvider
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
+import net.essentuan.esl.future.api.CompletionException
+import net.essentuan.esl.future.api.Future
 import net.essentuan.esl.iteration.extensions.toTypedArray
 import net.essentuan.esl.reflections.Reflections
 import net.essentuan.esl.reflections.extensions.instance
@@ -16,21 +24,18 @@ import net.essentuan.esl.scheduling.annotations.isAutoLoaded
 import net.fabricmc.loader.api.ModContainer
 import net.fabricmc.loader.api.metadata.ModMetadata
 import net.fabricmc.loader.api.metadata.ModOrigin
-import net.minecraft.ChatFormatting
-import net.minecraft.core.Registry
-import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.resources.ResourceLocation
-import net.minecraft.sounds.SoundEvent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.nio.file.Path
 import java.util.Optional
+import kotlin.system.exitProcess
 
 @Scan("com.busted_moments.client")
 object Client : Extension, ModContainer, Logger by LoggerFactory.getLogger("fuy_gg") {
     private lateinit var container: ModContainer
 
     @Override
+    @OptIn(DelicateCoroutinesApi::class)
     override fun init() {
         container = FabricLoader
             .getModContainer("fuy_gg")
@@ -43,27 +48,21 @@ object Client : Extension, ModContainer, Logger by LoggerFactory.getLogger("fuy_
                 .map { it.entrypoint }
                 .onEach {
                     it.sounds.forEach { key ->
-                        val location = ResourceLocation(key)
-
-                        Registry.register(
-                            BuiltInRegistries.SOUND_EVENT,
-                            location,
-                            SoundEvent.createVariableRangeEvent(location)
-                        )
+                        Sounds += key
                     }
                 }
                 .flatMap { it.scan.asIterable() }
                 .toTypedArray()
         )
+        Buster
 
         Scheduler.apply {
             capacity = 50
-            @OptIn(DelicateCoroutinesApi::class)
             DISPATCHER = GlobalScope
             this += this@Client
 
             start()
-        }
+        } finally { exitProcess(0) }
 
         Reflections.types.forEach {
             it.java.events.register()
@@ -73,6 +72,9 @@ object Client : Extension, ModContainer, Logger by LoggerFactory.getLogger("fuy_
         }
 
         Config.read()
+        Keybind.register()
+        Function.register()
+        Marker.Provider.register()
     }
 
     override fun getMetadata(): ModMetadata =
@@ -90,9 +92,15 @@ object Client : Extension, ModContainer, Logger by LoggerFactory.getLogger("fuy_
     override fun getContainedMods(): Collection<ModContainer> =
         container.containedMods
 
+    @Deprecated("Deprecated in Java")
     override fun getRootPath(): Path =
         container.rootPath
 
+    @Deprecated("Deprecated in Java")
     override fun getPath(file: String): Path =
         container.getPath(file)
+}
+
+inline fun <T> inline(crossinline block: suspend () -> T): Future<T> = Future(block).also {
+    it.except { ex -> Client.error("Exception in future!", CompletionException(it, cause = ex)) }
 }
