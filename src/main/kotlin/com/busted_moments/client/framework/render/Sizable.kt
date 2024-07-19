191af6a3c33f9039a2c7355d9b4629a1ca8ac03f
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-07-19 17:54:30 -0500

    Change usages of 'Floats' to FloatPair

--- src/main/kotlin/com/busted_moments/client/framework/render/Sizable.kt
+++ src/main/kotlin/com/busted_moments/client/framework/render/Sizable.kt
@@ -1,24 +1,24 @@
 package com.busted_moments.client.framework.render
 
-import com.busted_moments.client.framework.render.helpers.Floats
 import com.busted_moments.client.framework.render.helpers.Percentage
+import net.essentuan.esl.tuples.numbers.FloatPair
 
 interface Sizable {
     val width: Float
     val height: Float
 }
 
-infix fun Percentage.of(sizeable: Sizable): Floats = Floats(
+infix fun Percentage.of(sizeable: Sizable): FloatPair = FloatPair(
     sizeable.width * factor,
     sizeable.height * factor
 )
 
-operator fun Percentage.plus(sizable: Sizable): Floats = Floats(
+operator fun Percentage.plus(sizable: Sizable): FloatPair = FloatPair(
     sizable.width / factor,
     sizable.height / factor
 )
 
-operator fun Sizable.plus(pct: Percentage): Floats = Floats(
+operator fun Sizable.plus(pct: Percentage): FloatPair = FloatPair(
     width / pct.factor,
     height / pct.factor
 )
