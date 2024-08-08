f7cc66b9cf3bfb49fd17f7bd7e2ede4be99c1ac2
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-08-07 19:29:41 -0500

    Fix ClickToCongratulateFeature causing disconnect

--- src/main/kotlin/com/busted_moments/client/features/ClickToCongratulateFeature.kt
+++ src/main/kotlin/com/busted_moments/client/features/ClickToCongratulateFeature.kt
@@ -1,26 +1,20 @@
 package com.busted_moments.client.features
 
-import com.busted_moments.buster.api.PlayerType
 import com.busted_moments.buster.api.Profile
+import com.busted_moments.client.Patterns
 import com.busted_moments.client.framework.config.annotations.Tooltip
 import com.busted_moments.client.framework.config.entries.value.Value
 import com.busted_moments.client.framework.events.Subscribe
 import com.busted_moments.client.framework.features.Feature
 import com.busted_moments.client.framework.text.Text.component
-import com.busted_moments.client.framework.text.Text.invoke
 import com.busted_moments.client.framework.text.Text.matches
-import com.busted_moments.client.framework.text.get
 import com.busted_moments.client.framework.text.getValue
+import com.busted_moments.client.framework.artemis.message
+import com.busted_moments.client.framework.text.Text
 import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent
 import net.minecraft.network.chat.ClickEvent
-import java.util.regex.Pattern
 
 object ClickToCongratulateFeature : Feature() {
-    private val REGULAR_PATTERN: Pattern =
-        Pattern.compile("^\\[!] Congratulations to (?<player>.+) for reaching (?<type>.+) level (?<level>.+)!")
-    private val PROF_PATTERN: Pattern =
-        Pattern.compile("\\[!] Congratulations to (?<player>.+) for reaching level (?<level>.+) in (. )?(?<type>.+)!")
-
     @Value("Message")
     @Tooltip(["The message to send when congratulating people!"])
     private var message: String = "Congratulations!"
@@ -28,16 +22,19 @@ object ClickToCongratulateFeature : Feature() {
     @Subscribe
     private fun ChatMessageReceivedEvent.on() {
         originalStyledText.matches {
-            any(PROF_PATTERN, REGULAR_PATTERN) { matcher, text ->
+            any(
+                Patterns.LEVEL_UP,
+                Patterns.PROF_LEVEL_UP
+            ) { matcher, it ->
                 val player by matcher
                 if (!Profile.isValid(player ?: return))
                     return
 
-                message = component {
-                    +text
+                this@on.message = Text {
+                    +it
                     newLine()
 
-                    +"Click to congratulate ".aqua.underline.onClick(ClickEvent.Action.RUN_COMMAND, "/msg $player $message")
+                    +"Click to congratulate ".aqua.underline.onClick(ClickEvent.Action.RUN_COMMAND, "/msg $player ${this@ClickToCongratulateFeature.message}")
                     +player!!
                     +"!"
                 }
