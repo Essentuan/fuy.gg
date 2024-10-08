9c5cd20fdca9c7e0ac2d989d506707873c88681a
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-10-08 00:18:21 -0500

    Revert changes to line rendering
    I broke this accidentally when moving it to the new vertex builders; I'll fix it later.

--- src/main/kotlin/com/busted_moments/client/framework/render/Rendering.kt
+++ src/main/kotlin/com/busted_moments/client/framework/render/Rendering.kt
@@ -1,10 +1,12 @@
 package com.busted_moments.client.framework.render
 
-import com.busted_moments.client.framework.render.builder.quad
-import com.busted_moments.client.framework.render.builder.upload
-import com.busted_moments.client.framework.render.builder.vertex
 import com.busted_moments.client.framework.render.helpers.Context
+import com.mojang.blaze3d.vertex.VertexConsumer
+import com.wynntils.utils.render.buffered.CustomRenderType
 import net.essentuan.esl.color.Color
+
+fun VertexConsumer.setColor(color: Color): VertexConsumer =
+    setColor(color.red, color.green, color.blue, color.alpha)
 
 fun Context.line(
     color: Color,
@@ -14,80 +16,75 @@ fun Context.line(
     y2: Float,
     width: Float
 ) {
-    upload(this) {
-        var x1 = x1
-        var y1 = y1
-        var x2 = x2
-        var y2 = y2
-        val halfWidth = width / 2.0f
-        var tmp: Float
+    var x1 = x1
+    var y1 = y1
+    var x2 = x2
+    var y2 = y2
+    val matrix = pose.last().pose()
+    val halfWidth = width / 2.0f
+    val buffer = buffer.getBuffer(CustomRenderType.POSITION_COLOR_TRIANGLE_STRIP)
 
-        when {
-            x1 == x2 -> {
-                if (y2 < y1) {
-                    tmp = y1
-                    y1 = y2
-                    y2 = tmp
-                }
-
-                quad {
-                    vertex(x1 - halfWidth, y1, 0f, color = color)
-                    vertex(x2 - halfWidth, y2, 0f, color = color)
-                    vertex(x1 + halfWidth, y1, 0f, color = color)
-                    vertex(x2 + halfWidth, y2, 0f, color = color)
-                }
+    var tmp: Float
+    when {
+        x1 == x2 -> {
+            if (y2 < y1) {
+                tmp = y1
+                y1 = y2
+                y2 = tmp
             }
 
-            y1 == y2 -> {
-                if (x2 < x1) {
-                    tmp = x1
-                    x1 = x2
-                    x2 = tmp
-                }
+            buffer.addVertex(matrix, x1 - halfWidth, y1, 0f).setColor(color)
+            buffer.addVertex(matrix, x2 - halfWidth, y2, 0f).setColor(color)
+            buffer.addVertex(matrix, x1 + halfWidth, y1, 0f).setColor(color)
+            buffer.addVertex(matrix, x2 + halfWidth, y2, 0f).setColor(color)
+        }
 
-                quad {
-                    vertex(x1, y1 - halfWidth, 0f, color = color)
-                    vertex(x1, y1 + halfWidth, 0f, color = color)
-                    vertex(x2, y2 - halfWidth, 0f, color = color)
-                    vertex(x2, y2 + halfWidth, 0f, color = color)
-                }
+        y1 == y2 -> {
+            if (x2 < x1) {
+                tmp = x1
+                x1 = x2
+                x2 = tmp
             }
 
-            x1 < x2 && y1 < y2 || x2 < x1 && y2 < y1 -> {
-                if (x2 < x1) {
-                    tmp = x1
-                    x1 = x2
-                    x2 = tmp
-                    tmp = y1
-                    y1 = y2
-                    y2 = tmp
-                }
+            buffer.addVertex(matrix, x1, y1 - halfWidth, 0f).setColor(color)
+            buffer.addVertex(matrix, x1, y1 + halfWidth, 0f).setColor(color)
+            buffer.addVertex(matrix, x2, y2 - halfWidth, 0f).setColor(color)
+            buffer.addVertex(matrix, x2, y2 + halfWidth, 0f).setColor(color)
+        }
 
-                quad {
-                    vertex(x1 + halfWidth, y1 - halfWidth, 0f, color = color)
-                    vertex(x1 - halfWidth, y1 + halfWidth, 0f, color = color)
-                    vertex(x2 + halfWidth, y2 - halfWidth, 0f, color = color)
-                    vertex(x2 - halfWidth, y2 + halfWidth, 0f, color = color)
-                }
+        x1 < x2 && y1 < y2 || x2 < x1 && y2 < y1 -> {
+            if (x2 < x1) {
+                tmp = x1
+                x1 = x2
+                x2 = tmp
+                tmp = y1
+                y1 = y2
+                y2 = tmp
             }
 
-            else -> {
-                if (x1 < x2) {
-                    tmp = x1
-                    x1 = x2
-                    x2 = tmp
-                    tmp = y1
-                    y1 = y2
-                    y2 = tmp
-                }
+            buffer.addVertex(matrix, x1 + halfWidth, y1 - halfWidth, 0f).setColor(color)
 
-                quad {
-                    vertex(x1 + halfWidth, y1 + halfWidth, 0f, color = color)
-                    vertex(x1 - halfWidth, y1 - halfWidth, 0f, color = color)
-                    vertex(x2 + halfWidth, y2 + halfWidth, 0f, color = color)
-                    vertex(x2 - halfWidth, y2 - halfWidth, 0f, color = color)
-                }
+            buffer.addVertex(matrix, x1 - halfWidth, y1 + halfWidth, 0f).setColor(color)
+
+            buffer.addVertex(matrix, x2 + halfWidth, y2 - halfWidth, 0f).setColor(color)
+
+            buffer.addVertex(matrix, x2 - halfWidth, y2 + halfWidth, 0f).setColor(color)
+        }
+
+        else -> {
+            if (x1 < x2) {
+                tmp = x1
+                x1 = x2
+                x2 = tmp
+                tmp = y1
+                y1 = y2
+                y2 = tmp
             }
+
+            buffer.addVertex(matrix, x1 + halfWidth, y1 + halfWidth, 0f).setColor(color)
+            buffer.addVertex(matrix, x1 - halfWidth, y1 - halfWidth, 0f).setColor(color)
+            buffer.addVertex(matrix, x2 + halfWidth, y2 + halfWidth, 0f).setColor(color)
+            buffer.addVertex(matrix, x2 - halfWidth, y2 - halfWidth, 0f).setColor(color)
         }
     }
 }
