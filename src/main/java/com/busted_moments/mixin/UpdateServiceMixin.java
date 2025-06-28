8923e4f13ef47714c3d27122ea7e41939e38e022
Essentuan <51426319+Essentuan@users.noreply.github.com>
2025-06-28 13:22:58 -0500

    Fix UpdateServiceMixin for 3.1

--- src/main/java/com/busted_moments/mixin/UpdateServiceMixin.java
+++ src/main/java/com/busted_moments/mixin/UpdateServiceMixin.java
@@ -3,6 +3,7 @@ package com.busted_moments.mixin;
 
 import com.busted_moments.client.framework.FabricLoader;
 import com.wynntils.services.athena.UpdateService;
+import com.wynntils.services.athena.type.UpdateResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,10 +14,10 @@ import java.util.concurrent.CompletableFuture;
 @Mixin(value = UpdateService.class, remap = false)
 public abstract class UpdateServiceMixin {
     @Inject(method = "tryUpdate", at = @At("HEAD"), cancellable = true)
-    private void tryUpdate(CallbackInfoReturnable<CompletableFuture<UpdateService.UpdateResult>> cir) {
+    private void tryUpdate(CallbackInfoReturnable<CompletableFuture<UpdateResult>> cir) {
         try {
             if (FabricLoader.INSTANCE.isModLoaded("fuy_gg_loader"))
-                cir.setReturnValue(CompletableFuture.completedFuture(UpdateService.UpdateResult.SUCCESSFUL));
+                cir.setReturnValue(CompletableFuture.completedFuture(UpdateResult.SUCCESSFUL));
         } catch (Exception e) {
             //Ignored
         }
