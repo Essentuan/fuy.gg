7c639be1f3effbcfc87e7f35eba4a74a4a40c386
Essentuan <51426319+Essentuan@users.noreply.github.com>
2025-09-20 18:44:46 -0500

    Update PersistedValueMixin.java

--- src/main/java/com/busted_moments/mixin/PersistedValueMixin.java
+++ src/main/java/com/busted_moments/mixin/PersistedValueMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = PersistedValue.class, remap = false)
 public abstract class PersistedValueMixin {
    @SuppressWarnings("unchecked")
-   @Inject(method = "setRaw", at = @At("HEAD"), cancellable = true)
+   @Inject(method = "setWithoutTouch", at = @At("HEAD"), cancellable = true)
    private void setRaw(Object value, CallbackInfo ci) {
       if (((Object) this) instanceof LinkedConfig<?>) {
          ((LinkedConfig<Object>) (Object) this).store(value);
