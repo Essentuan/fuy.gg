6a7104c5b10721cac85e9e0c1130d2e34cc73945
Essentuan <51426319+Essentuan@users.noreply.github.com>
2025-10-17 15:07:03 -0500

    More minor fixes

--- src/main/kotlin/com/busted_moments/client/features/chat/StackDuplicateMessagesFeature.kt
+++
@@ -1,7 +0,0 @@
-package com.busted_moments.client.features.chat
-
-import com.busted_moments.client.framework.config.annotations.Category
-import com.busted_moments.client.framework.features.Feature
-
-@Category("Chat")
-object  StackDuplicateMessagesFeature : Feature()
